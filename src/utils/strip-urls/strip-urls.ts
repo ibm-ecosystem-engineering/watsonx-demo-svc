10ecdf6a42cd294e6ec887a752d3ff388bb621f0
Sean Sundberg <seansund@us.ibm.com>
2023-11-07 22:06:18 -0600

    Replace stripTags module with local utility (#148)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- src/utils/strip-urls/strip-urls.ts
+++ src/utils/strip-urls/strip-urls.ts
@@ -1,3 +1,6 @@
 export const stripUrls = (text: string): string => {
-    return text.replace(/https?:\/\/[\n\S]+/g, '');;
+    return text
+        .replace(/https?:\/\/[\n\S]+/g, '')
+        .replace(/[0-9A-Za-z]+\/[0-9A-Za-z]+\/[0-9A-Za-z\/]+/, '')
+        .replace(/([ (])\/[0-9A-Za-z\/]+/, '$1')
 }
